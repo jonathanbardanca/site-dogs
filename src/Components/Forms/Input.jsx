de917cf1b856633006ef79b06b674f0d666d08f3
Jonathan Bardança <jonathanbardanca1@gmail.com>
2025-11-21 01:54:58 -0300

    feat(forms): add custom useForms hook for form validation

--- src/Components/Forms/Input.jsx
+++ src/Components/Forms/Input.jsx
@@ -1,13 +1,24 @@
 import React from "react";
 import styles from "./Input.module.css";
 
-const Input = ({ label, type }) => {
+const Input = ({ label, type, name, value, onChange, error, onBlur }) => {
   return (
     <div className={styles.wrapper}>
-      <label className={styles.label} htmlFor={label}></label>
-      <input className={styles.input} type={type} />
-      <p className={styles.error}>Error</p>
+      <label className={styles.label} htmlFor={name}>
+        {label}
+      </label>
+      <input
+        className={styles.input}
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        onBlur={onBlur}
+      />
+      {error && <p className={styles.error}>{error}</p>}
     </div>
-)};
+  );
+};
 
 export default Input;
