c24aa3873368cd5080303bd1faebc2120fb4ca71
Jonathan Bardança <jonathanbardanca1@gmail.com>
2025-11-21 17:41:54 -0300

    feat(context): create global app context and move shared state (auth, loading, error, data)

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,26 @@
-import React from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
-import Footer from "./Components/Footer";
-import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Login from "./Components/Login/Login";
+import React from 'react';
+import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Header from './Components/Header';
+import Footer from './Components/Footer';
+import Home from './Components/Home';
+import Login from './Components/Login/Login';
+import { UserStorage } from './UserContext';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/login/*" element={<Login />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
+    <div>
+      <BrowserRouter>
+        <UserStorage>
+          <Header />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/login/*" element={<Login />} />
+          </Routes>
+          <Footer />
+        </UserStorage>
+      </BrowserRouter>
+    </div>
   );
 }
 
--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,18 +1,27 @@
-import React from "react";
-import styles from "./Header.module.css";
-import { Link } from "react-router-dom";
-import Dogs from "../Assets/dogs.svg";
+import React from 'react';
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
+import Dogs from '../Assets/dogs.svg?react';
+import { UserContext } from '../UserContext';
 
 const Header = () => {
+  const { data } = React.useContext(UserContext);
+
   return (
     <header className={styles.header}>
       <nav className={`${styles.nav} container`}>
-        <Link to="/" className={styles.logo} aria-label="Dogs - Home">
-          <img src={Dogs} alt="Dogs" />
+        <Link className={styles.logo} to="/" aria-label="Dogs - Home">
+          <Dogs />
         </Link>
-        <Link to="/Login" className={styles.login}>
-          Login / Criar
-        </Link>
+        {data ? (
+          <Link className={styles.login} to="/conta">
+            {data.nome}
+          </Link>
+        ) : (
+          <Link className={styles.login} to="/login">
+            Login / Criar
+          </Link>
+        )}
       </nav>
     </header>
   );
