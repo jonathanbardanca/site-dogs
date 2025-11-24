fa966e66bc82b2c3330938c13bca87007f2dda83
Jonathan Bardança <jonathanbardanca1@gmail.com>
2025-11-24 06:05:57 -0300

    feat(auth): start user creation logic and update LoginCreate UI

--- src/Hooks/useForm.jsx
+++ src/Hooks/useForm.jsx
@@ -6,10 +6,15 @@ const types = {
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
     message: "Preencha um email válido",
   },
+  password: {
+    regex: /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z]{8,}$/,
+    message:
+      "A senha precisa ter 1 caracter maíusculo, 1 minúsculo e 1 digito. Com no mínimo 8 caracteres.",
+  },
 };
 
 const useForm = (type) => {
-  const [value, setValue] = React.useState('');
+  const [value, setValue] = React.useState("");
   const [error, setError] = React.useState(null);
 
   function validate(value) {
@@ -27,7 +32,7 @@ const useForm = (type) => {
   }
 
   function onChange({ target }) {
-    if (error) validate(target.value); 
+    if (error) validate(target.value);
     setValue(target.value);
   }
 
